5c3931c2c120b16a4885fcc88f67f4f383d502aa
Krishna <89905895+krishna7288@users.noreply.github.com>
2024-03-15 10:08:06 +0530

    Facade

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Linear } from "./components/Products/Linear";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/common/Home";
 import Outdoor from "./components/Products/Outdoor";
+import { Facade } from "./components/Products/Facade";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route element={<Home />}>
           <Route path="/" element={<Linear />} />
           <Route path="/products/outdoorlights" element={<Outdoor />} />
+          <Route path="/products/facadelights" element={<Facade />} />
         </Route>
       </Routes>
     </Router>
