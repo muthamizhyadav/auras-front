853e8633717ba107735901c34ffd73a599f934fc
Wasimwhy <wasim@whyglobalservices.com>
2024-04-03 16:47:18 +0530

    searchbar style changed

--- src/components/common/MoreProducts.jsx
+++ src/components/common/MoreProducts.jsx
@@ -34,29 +34,37 @@ export const MoreProducts = () => {
 
   return (
     <div>
-      {/* <input
-        type="text"
-        value={searchQuery}
-        onChange={handleSearchInputChange}
-        placeholder="Search products..."
-      /> */}
-      <div className="w-full bg-moreproductsbg bg-cover h-[300px] flex justify-center items-center">
-      <div className="group">
-        <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
-          <g>
-            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-          </g>
-        </svg>
-        <input
-          placeholder="Search products..."
-          type="text"
-          className="input sm:w-96 w-full placeholder:text-sm"
-          value={searchQuery}
-          onChange={handleSearchInputChange}
-        />
+      <div className="relative w-full bg-moreproductsbg bg-cover h-[300px] flex justify-center items-center">
+        <div className="absolute inset-0 bg-black opacity-30"></div>
+
+        <form className="form">
+          <button>
+            <svg
+              width="17"
+              height="16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-labelledby="search"
+            >
+              <path
+                d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+                stroke="currentColor"
+                strokeWidth="1.333"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+          </button>
+          <input
+            placeholder="Search products..."
+            type="text"
+            className="input sm:w-96 w-full placeholder:text-sm"
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+          />
+        </form>
       </div>
-      </div>
-      
 
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 place-items-center gap-y-5 py-5">
         {filteredProducts.map((item) => (
