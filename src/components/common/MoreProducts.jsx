cd54d4e8f5af3d0503f945e7e02e050b3c1f90f4
Wasimwhy <wasim@whyglobalservices.com>
2024-04-04 15:56:49 +0530

    Search issue Solved

--- src/components/common/MoreProducts.jsx
+++ src/components/common/MoreProducts.jsx
@@ -5,6 +5,7 @@ import Sanityclient from "./Sanityclient";
 export const MoreProducts = () => {
   const [model, setModel] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [filter,setfilter] = useState('')
   const { id } = useParams();
 
   useEffect(() => {
@@ -14,29 +15,31 @@ export const MoreProducts = () => {
       .then((data) => {
         console.log(data);
         setModel(data);
+        setfilter(data);
       })
       .catch((error) => {
         console.error("Error fetching content:", error);
       });
   }, [id]);
 
-  const handleSearchInputChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  if (!model || model.length === 0) {
-    return <div>No products found.</div>;
-  }
-
-  const filteredProducts = model.filter((item) =>
-    item.modelid.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const handleSearchInputChange = (e) => {
+    setfilter([])
+     setSearchQuery(e.target.value);
+     if (e.target.value==="") {
+         setfilter(model);
+         return;
+     }
+     const filteredResults = model.filter(
+         (data) =>
+           data.modelid.toLowerCase().includes(e.target.value.toLowerCase())
+     );
+     setfilter(filteredResults);
+ };
 
   return (
     <div>
       <div className="relative w-full bg-moreproductsbg bg-cover h-[300px] flex justify-center items-center">
         <div className="absolute inset-0 bg-black opacity-30"></div>
-
         <form className="form">
           <button>
             <svg
@@ -66,24 +69,34 @@ export const MoreProducts = () => {
         </form>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 place-items-center gap-y-5 py-5">
-        {filteredProducts.map((item) => (
-          <Link
-            key={item.modelid}
-            to={`/home/products/indoorlights/${item.modelid.toLowerCase()}`}
-            className="flex justify-center items-center relative p-2 sm:w-44 sm:h-48 bg-white drop-shadow-md text-xs hover:text-[#F2667C]"
-          >
-            {item.modelimage && item.modelimage.asset && (
-              <img
-                src={item.modelimage.asset.url}
-                alt=""
-                className="hover:scale-110 transition-all duration-300"
-              />
-            )}
-            <p className="absolute bottom-2">{item.modelid}</p>
-          </Link>
-        ))}
-      </div>
+      {filter.length === 0 ? (
+        <div className="bg-red-500 w-full text-center p-10 text-xl text-white">No Product Found!</div>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center gap-y-5 py-5">
+          {filter.map((item) => (
+            <Link
+              key={item.modelid}
+              to={`/home/products/indoorlights/${item.modelid.toLowerCase()}`}
+              className="flex flex-col justify-evenly items-center relative p-2 sm:w-52 sm:h-48 bg-white drop-shadow-md text-xs hover:text-[#F2667C]"
+            >
+              {item.modelimage && item.modelimage.asset && (
+                <img
+                  src={item.modelimage.asset.url}
+                  alt=""
+                  className="hover:scale-110 transition-all w-40 duration-300 p-2 "
+                />
+              )}
+              <p className="">{item.modelid}</p>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
+
+
+
+
+
+
